e54c4497bd8150cf78fd362048447da7eb196644
andrew wong <awwong92@gmail.com>
2018-04-24 15:58:27 -0400

    secured /api/user route with simple conditional

--- server/api/users.js
+++ server/api/users.js
@@ -3,12 +3,16 @@ const {User} = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
-  User.findAll({
-    // explicitly select only the id and email fields - even though
-    // users' passwords are encrypted, it won't help if we just
-    // send everything to anyone who asks!
-    attributes: ['id', 'email']
-  })
+  if (req.user !== undefined && req.user.isAdmin){
+    User.findAll({
+      // explicitly select only the id and email fields - even though
+      // users' passwords are encrypted, it won't help if we just
+      // send everything to anyone who asks!
+      attributes: ['id', 'email']
+    })
     .then(users => res.json(users))
     .catch(next)
+  } else {
+    res.status(404).send('Admin privileges are required to view this information.')
+  }
 })
