884745f575ddd8ad86145b88822fa7a5d9ad06e6
Frances Wang <francesyuanwang@gmail.com>
2018-04-23 21:40:42 -0400

    Updated tests for the cart store

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,7 +1,7 @@
 /* global describe beforeEach afterEach it */
 
 import {expect} from 'chai'
-import {fetchCart} from './cart'
+import {fetchCart, addProductToCart} from './cart'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
@@ -41,13 +41,13 @@ describe('thunk creators', () => {
 
   describe('add to cart', () => {
     it('dispatches the ADD TO CART action', () => {
-      const fakeCart = { 3: 5, 7: 1 }
-      mockAxios.onGet('/api/cart').replyOnce(200, fakeCart)
-      return store.dispatch(fetchCart())
+      const fakeCart = { 6: 3 }
+      mockAxios.onPut('/api/cart/add', {productId: 6}).replyOnce(200, { 6: fakeCart[6] + 1 })
+      return store.dispatch(addProductToCart({productId: 6}))
         .then(() => {
           const actions = store.getActions()
-          expect(actions[0].type).to.be.equal('GET_CART')
-          expect(actions[0].cart).to.be.deep.equal(fakeCart)
+          expect(actions[0].type).to.be.equal('ADD_TO_CART')
+          expect(actions[0].product).to.be.deep.equal({6: 4})
         })
     })
   })
