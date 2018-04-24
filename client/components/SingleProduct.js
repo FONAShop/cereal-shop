cf843ca7e46fbf69fae1be475737b7baa2eb59c8
Frances Wang <francesyuanwang@gmail.com>
2018-04-24 13:04:10 -0400

    Updated single product style

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Reviews from './Reviews';
-
 import { connect } from 'react-redux'
 import { addProductToCart } from '../store'
+import { Container, Header, Label, Button, Icon, Grid, Segment, Image } from 'semantic-ui-react';
+
 
 class SingleProduct extends Component {
   constructor(props){
@@ -14,19 +15,29 @@ class SingleProduct extends Component {
     const productDetails = this.props.selectedProduct
     const { addButtonClick } = this.props;
     return (
-      <div>
+      <Container>
         { productDetails && (
             <div key={productDetails.id}>
-              <img src={productDetails.imgUrl} />
-              <div>{'Title: ' + productDetails.name}</div>
-              <div>{'Price: ' + productDetails.price}</div>
-              <div>{'Desc: ' + productDetails.description}</div>
-              <button name={productDetails.id} onClick={(e) => addButtonClick(e)}>Add to cart</button>
+            <Segment style={{ padding: '5em 0em' }}>
+              <Grid container stackable>
+              <Grid.Row>
+              <Grid.Column width={6}>
+              <Image src={productDetails.imgUrl} rounded size="large" />
+              </Grid.Column>
+              <Grid.Column floated="right" width={8}>
+              <Header as="h1">{productDetails.name}</Header>
+              <Header as="h2">{'$' + productDetails.price}</Header>
+              <p style={{ fontSize: '1.33em' }}>{'Description: ' + productDetails.description}</p>
+              <Button name={productDetails.id} onClick={addButtonClick}>Add to cart</Button>
+              </Grid.Column>
+              </Grid.Row>
+              </Grid>
+              </Segment>
             <Reviews productId={productDetails.id} />
             </div>
           )
         }
-      </div>
+      </Container>
     );
   }
 }
