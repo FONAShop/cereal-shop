4295f137b34043960ce1914a29f4c47b76517e44
Frances Wang <francesyuanwang@gmail.com>
2018-04-25 10:32:39 -0400

    show categories

--- client/components/Product.js
+++ client/components/Product.js
@@ -9,6 +9,13 @@ const Product = ({ product, addButtonClick }) => (
     <a href={`product/${product.id}`}><Image rounded height="300" src={product.imgUrl} /></a>
     <Card.Content>
     <Card.Header>{product.name}</Card.Header>
+    <Card.Meta>
+    { product.categories.map((category, idx) => {
+      return (
+        <Button basic color="brown" size="mini" key={idx} style={{ padding: '0.4em 0.6em 0.6em', margin: '0em 0.3em'}}>{category}</Button>
+      );
+    })}
+    </Card.Meta>
     <Label tag color="green" style={{ marginTop: '0.5em'}}>{'$' + product.price}</Label>
     <Button dataname={product.id} onClick={addButtonClick} floated="right" size="small">
       <Icon dataname={product.id} name="add to cart" />
