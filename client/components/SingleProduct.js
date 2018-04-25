b604e8f91b71f61603ed2615df74347a308490d9
Frances Wang <francesyuanwang@gmail.com>
2018-04-24 22:34:34 -0400

    Updated

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Reviews from './Reviews';
 import { connect } from 'react-redux'
 import { addProductToCart } from '../store'
